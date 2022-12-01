41c4c75e4d6381c29d533b883c768fe986794e0f
J.J. Jones <jacob.j.jones01@gmail.com>
2022-12-01 16:00:31 -0600

    Finished Project

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,23 @@
 package org.example;
 
+import org.apache.commons.lang.StringUtils;
+
+import java.util.Scanner;
+
 public class Main {
+    private final Scanner scan = new Scanner(System.in);
+
+    public Main(){
+        System.out.println("Enter something");
+        String input = scan.next();
+        System.out.printf("\"%s\" contains integer = %b \n",input, StringUtils.isNumeric(input));
+        System.out.printf("\"%s\" inverted case = %s \n",input, StringUtils.swapCase(input));
+        System.out.printf("\"%s\" reversed = %s \n",input, StringUtils.reverse(input));
+        input = StringUtils.reverse(input);
+        System.out.printf("\"%s\" inverted reversed case = %s \n",input, StringUtils.swapCase(input));
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        new Main();
     }
 }
